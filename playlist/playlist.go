76a94ba26f82a193af6f24d0be9d11ea529e06fe
nryberg <nryberg@gmail.com>
2017-11-30 17:06:22 -0600

    Adding list of stations - timestamp for scan

--- playlist/playlist.go
+++ playlist/playlist.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
@@ -11,34 +14,76 @@ import (
 
 // PlayedSong is a track played
 type PlayedSong struct {
+	scanTime    string
+	station     string
+	playDate    string
+	playTime    string
 	trackTitle  string
 	trackArtist string
-	playTime    string
-	playDate    string
 	contentID   string
 }
 
 func main() {
-	log.Println("Playlist Start")
-	url := "https://kool108.iheart.com/music/recently-played/"
-	url = "https://957bigfm.iheart.com/music/recently-played/"
-	results, err := ReadTracks(url)
+	const baseURLPrefix = "https://"
+	const baseURLPostfix = ".iheart.com/music/recently-played/"
+	var url string
+
+	log.Println("Playlist Starter")
+
+	stations, err := GetStations("stations.txt")
+	log.Println(len(stations))
 	if err != nil {
-		panic(err)
+		log.Println(err)
+	} else {
+
+		for _, station := range stations {
+			log.Println(station)
+			url = baseURLPrefix + station + baseURLPostfix
+			log.Println(url)
+			results, err := ReadTracks(url, station)
+			if err != nil {
+				panic(err)
+			}
+			log.Println(len(results))
+
+			PlaysToStdout(results)
+		}
+
 	}
-	log.Println(len(results))
 
-	PlaysToStdout(results)
+}
 
+// GetStations pulls the current list of stations
+func GetStations(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		//log.Printf(os.Stderr, err)
+		log.Println(err)
+	}
+
+	return lines, err
 }
 
 // PlaysToStdout sends them to stdout
-func PlaysToStdout([]PlayedSong) {
-	log.Println("Done")
+func PlaysToStdout(plays []PlayedSong) {
+	//var play PlayedSong
+	for _, play := range plays {
+		log.Println(play.station, play.trackTitle)
+	}
 }
 
 // ReadTracks accepts the url for fetching data and goes and gets it
-func ReadTracks(url string) ([]PlayedSong, error) {
+func ReadTracks(url string, station string) ([]PlayedSong, error) {
 	var playResults []PlayedSong
 
 	res, err := http.Get(url)
@@ -54,6 +99,10 @@ func ReadTracks(url string) ([]PlayedSong, error) {
 	if err != nil {
 		panic(err)
 	}
+
+	var scanTime string
+	t := time.Now()
+	scanTime = t.Format(time.RFC850)
 
 	var playedDate string
 
@@ -71,7 +120,9 @@ func ReadTracks(url string) ([]PlayedSong, error) {
 	// grab all articles and print them
 	plays := scrape.FindAll(root, scrape.ByClass("playlist-track-container"))
 	for _, play := range plays {
+		played.scanTime = scanTime
 		played.playDate = playedDate
+		played.station = station
 		played.contentID = scrape.Attr(play, "data-contentid")
 		trackTitleNode, ok := scrape.Find(play, scrape.ByClass("track-title"))
 		if ok {
